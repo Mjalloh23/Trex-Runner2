b7233798afd4d7a50a9b98ce69d44bd77e588195
Mjalloh23 <89691774+Mjalloh23@users.noreply.github.com>
2021-09-04 13:32:46 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,52 @@
+var trex, trex_running, edges;
+var groundImage;
+var ground2;
+
+var ground
+
+function preload(){
+  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
+  groundImage = loadImage("ground2.png")
+}
+
+function setup(){
+  createCanvas(600,200);
+  
+  // creating trex
+  trex = createSprite(50,160,20,50);
+  trex.addAnimation("running", trex_running);
+  edges = createEdgeSprites();
+  
+  //adding scale and position to trex
+  ground=createSprite(300,180,600,10)
+  ground.addImage(groundImage)
+  ground.velocityX=-5
+  trex.scale = 0.5;
+  trex.x = 50
+
+  ground2=createSprite(300,200,600,10)
+  ground2.visible=false
+}
+
+
+function draw(){
+  //set background color 
+  background("yellow");
+  if(ground.x<0){
+   ground.x=300
+  }
+  //logging the y position of the trex
+  console.log(trex.y)
+  
+  //jump when space key is pressed
+  if(keyDown("space")){
+    trex.velocityY = -10;
+  }
+  
+  trex.velocityY = trex.velocityY + 0.5;
+  
+  //stop trex from falling down
+  trex.collide(ground2)
+
+  drawSprites();
+}
